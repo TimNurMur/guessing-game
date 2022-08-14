17ee1cbecd2350ec4c60e016ab1c5795e3386360
TimNurMur <tim.nur-mur@mail.ru>
2022-08-14 20:33:31 +0400

    feat: add code

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.value = Math.round((this.min + this.max) / 2);
     }
 
     lower() {
-
+        this.max = this.value;
     }
 
     greater() {
-
+        this.min = this.value;
     }
 }
 
